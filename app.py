b401f3b5c03063d32500daadd68c9d67186e102a
justincheng20 <justincheng20@gmail.com>
2020-02-04 12:03:47 -0800

    Added features to register user and encrypt password

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, redirect
 # from flask_debugtoolbar import DebugToolbarExtension
 
 from models import db, connect_db, User
+from forms import RegisterForm
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
@@ -13,7 +15,41 @@ app.config["SQLALCHEMY_ECHO"] = True
 connect_db(app)
 db.create_all()
 
-app.config['SECRET)KEY'] = "SECRET KEY"
+bcrypt = Bcrypt()
 
-# @app.route("/")
-# def root():
+app.config['SECRET_KEY'] = "SECRET KEY"
+
+@app.route("/")
+def root():
+    return redirect("/register")
+
+
+@app.route("/register")
+def register_user_form():
+    form = RegisterForm()
+    return render_template("register.html", form=form)
+
+
+@app.route("/register", methods = ["POST"])
+def create_user():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+    
+    hashed_password = bcrypt.generate_password_hash("password")
+
+    new_user = User(username=username, 
+                    password=hashed_password, 
+                    email=email, 
+                    first_name=first_name, 
+                    last_name=last_name)
+    
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect("/secret")
---
+++ forms.py
@@ -0,0 +1,12 @@
+from wtforms import StringField, PasswordField
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired, Optional, Email
+
+class RegisterForm(FlaskForm):
+    """Form for adding a new user"""
+
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    email = StringField("Email", validators=[InputRequired(), Email()])
+    first_name = StringField("First Name", validators=[InputRequired()])
+    last_name = StringField("Last Name", validators=[InputRequired()])
